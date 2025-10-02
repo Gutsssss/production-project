c50de3d0651034355e1ad656e84dc65786fe0e51
Matvey M <kacill1@mail.ru>
2025-10-02 22:02:08 +0500

    add sidebar and style variables

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,17 @@ import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "app/providers/router";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,16 +1,21 @@
-import { Suspense } from "react"
-import { Routes, Route } from "react-router-dom"
-import { routerConfig } from "shared/config/routeConfig/routeConfig"
-
+import { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { routerConfig } from "shared/config/routeConfig/routeConfig";
 
 export const AppRouter = () => {
-    return (
-        <Suspense fallback={<div>...Loading</div>}>
-        <Routes>
-          {Object.values(routerConfig).map(({element,path}) => {
-            return <Route key={path} path={path} element={element}/>
-          })}
-        </Routes>
-      </Suspense>
-    )
-}
+  return (
+    <Suspense fallback={<div>...Loading</div>}>
+      <Routes>
+        {Object.values(routerConfig).map(({ element, path }) => {
+          return (
+            <Route
+              key={path}
+              path={path}
+              element={<div className="page-wrapper">{element}</div>}
+            />
+          );
+        })}
+      </Routes>
+    </Suspense>
+  );
+};
