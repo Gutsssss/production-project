33b7997e7e52c0b45cd966b31c97e2d7dfe93c7a
Matvey M <kacill1@mail.ru>
2025-10-06 20:05:24 +0500

    add notFoundPage comp, fix style files,add loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routerConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 export const AppRouter = () => (
-    <Suspense fallback={<div>...Loading</div>}>
+    <Suspense fallback={<PageLoader />}>
         <Routes>
             {Object.values(routerConfig).map(({ element, path }) => (
                 <Route
