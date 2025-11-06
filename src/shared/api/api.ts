e6bf32ab86785aa52911b2581dc479da00449fa2
Matvey M <kacill1@mail.ru>
2025-11-06 20:17:38 +0500

    add intercepros

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -6,5 +6,8 @@ export const $api = axios.create({
 });
 
 $api.interceptors.request.use((config) => {
-    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
